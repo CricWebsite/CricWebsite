2c1364659eda7000be9f454d0adb2c055c21b7ed
CricWebsite <84238000+CricWebsite@users.noreply.github.com>
2021-05-15 23:57:59 +0100

    Add files via upload

---
+++ logs/1.error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2021-03-08 19:00:51 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2021-03-08T19:00:51+00:00	INFO ::1	joomlafailure	Empty password not allowed.
---
+++ logs/joomla_update.php
@@ -0,0 +1,20 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2021-04-17 21:35:01 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2021-04-17T21:35:01+00:00	INFO ::1	update	Update started by user Super User (872). Old version is 3.9.24.
+2021-04-17T21:35:05+00:00	INFO ::1	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.9.26-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA6LXDJLNUINX2AVMH%2F20210417%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20210417T213500Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=e1997de9f8ab9f0e4a3ed56cb91fc11873e930d9de6b964db5bab3716aa1f1af.
+2021-04-17T21:35:08+00:00	INFO ::1	update	Update started by user Super User (872). Old version is 3.9.24.
+2021-04-17T21:35:10+00:00	INFO ::1	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.9.26-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA6LXDJLNUINX2AVMH%2F20210417%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20210417T213506Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=4232a10815e2725d01155f6ae729b2c9d20a0e11880f76c5bbaf821fe85aa3c5.
+2021-04-17T21:35:29+00:00	INFO ::1	update	File Joomla_3.9.26-Stable-Update_Package.zip downloaded.
+2021-05-01T14:48:51+00:00	INFO ::1	update	Update started by user Super User (872). Old version is 3.9.24.
+2021-05-01T14:48:54+00:00	INFO ::1	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.9.26-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA6LXDJLNUINX2AVMH%2F20210501%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20210501T144849Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=7479b9f30e0bfc5a10991cbaa43efd616c49732a569e6378a22596b43b015ddb.
+2021-05-01T14:49:00+00:00	INFO ::1	update	File Joomla_3.9.26-Stable-Update_Package.zip downloaded.
+2021-05-01T14:49:01+00:00	INFO ::1	update	Starting installation of new version.
+2021-05-01T14:54:06+00:00	INFO ::1	update	Finalising installation.
+2021-05-01T14:54:06+00:00	INFO ::1	update	Ran query from file 3.9.26-2021-04-07. Query text: INSERT INTO `#__postinstall_messages` (`extension_id`, `title_key`, `description.
+2021-05-01T14:54:06+00:00	INFO ::1	update	Deleting removed files and folders.
+2021-05-01T14:54:11+00:00	INFO ::1	update	Cleaning up after installation.
+2021-05-01T14:54:11+00:00	INFO ::1	update	Update to version 3.9.26 is complete.
